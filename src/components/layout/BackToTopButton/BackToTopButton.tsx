53b4e65b1bf93909cd15e65c0e8d50240dbac072
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-05-13 02:46:42 +0500

    add fix

--- src/components/layout/BackToTopButton/BackToTopButton.tsx
+++ src/components/layout/BackToTopButton/BackToTopButton.tsx
@@ -1,19 +1,23 @@
-import * as React from 'react';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styles from './BackToTopButton.module.scss';
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./BackToTopButton.module.scss";
 
 export const BackToTopButton = () => {
-    const backToTop = () => {
-        if (window.pageYOffset > 0) {
-            window.scrollBy(0, -80);
-            setTimeout(backToTop, 5);
-        }
-    };
+  const backToTop = () => {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 5);
+    }
+  };
 
-    return (
-        <button className={styles.root} title="Наверх" onClick={backToTop} type="button">
-            <FontAwesomeIcon icon={faArrowUp} className={styles.icon} />
-        </button>
-    );
+  return (
+    <button
+      className={styles.root}
+      title="Наверх"
+      onClick={backToTop}
+      type="button"
+    >
+      <FontAwesomeIcon icon={faArrowUp} className={styles.icon} />
+    </button>
+  );
 };
